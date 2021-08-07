53f6d71b1dabd5f2cdad0bba97cd93b0381a4a5b
sammiepls <samantha.lhe@gmail.com>
2021-08-07 22:05:46 +0800

    Add infinite scroll pagination

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,28 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        words: {
+          keyArgs: false,
+          merge(existing = { data: [] }, incoming) {
+            const mergedData = {
+              ...incoming,
+              data: [...existing.data, ...incoming.data],
+            };
+            return mergedData;
+          },
+        },
+      },
+    },
+  },
+});
+
 export const client = new ApolloClient({
   uri: "https://graphql.fauna.com/graphql",
   headers: {
     authorization: `Bearer ${process.env.REACT_APP_FAUNADB_KEY}`,
   },
-  cache: new InMemoryCache(),
+  cache,
 });
