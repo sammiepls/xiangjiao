62be329b9bc0592952210737f50d6a6c35333a86
sammiepls <samantha.lhe@gmail.com>
2021-08-28 21:52:45 +0800

    Remove option to search by eng/cn

--- src/pages/DictionaryPage.tsx
+++ src/pages/DictionaryPage.tsx
@@ -1,8 +1,8 @@
 import { ReactElement, useState, useEffect } from "react";
 import Search from "components/Search";
 import WordList from "components/WordList";
-import { useQuery, useLazyQuery } from "@apollo/react-hooks";
-import { GET_WORDS, GET_WORD_BY_CN, GET_WORD_BY_EN } from "graphql/queries";
+import { useQuery } from "@apollo/react-hooks";
+import { GET_WORDS } from "graphql/queries";
 import Loader from "components/Loader";
 import Pagination from "components/Pagination";
 
@@ -14,19 +14,10 @@ export default function Dictionary(): ReactElement {
   const [paginatedWords, setPaginatedWords] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
 
-  const [language, setLanguage] = useState<"en" | "cn">("en");
-  const isEn = language === "en";
   const { data, loading, error, fetchMore } = useQuery(GET_WORDS);
-
-  const [getSearchResults, { data: searchData, loading: searchLoading }] =
-    useLazyQuery(isEn ? GET_WORD_BY_EN : GET_WORD_BY_CN);
 
   let { words } = data || {};
   const { after } = words || {};
-
-  if (searchData) {
-    words = searchData[isEn ? "wordsByEn" : "wordsByCn"];
-  }
 
   const filterWords = (query) => {
     setSearchResults(
@@ -60,24 +51,6 @@ export default function Dictionary(): ReactElement {
     <>
       <div className="sticky md:static top-12 z-40 bg-lightYellow pb-4">
         <Search onSearch={filterWords} />
-        <div className="text-center mt-4">
-          <button
-            onClick={() => setLanguage("en")}
-            className={`p-2 rounded-full text-xs mr-2 ${
-              isEn ? "bg-yellow" : "bg-lightGray text-gray"
-            }`}
-          >
-            in english
-          </button>
-          <button
-            onClick={() => setLanguage("cn")}
-            className={`p-2 rounded-full text-xs mr-2 ${
-              !isEn ? "bg-yellow" : "bg-lightGray text-gray"
-            }`}
-          >
-            in chinese
-          </button>
-        </div>
       </div>
 
       {loading ? (
@@ -86,7 +59,7 @@ export default function Dictionary(): ReactElement {
         <>
           <WordList
             words={searchResults.length ? searchResults : paginatedWords}
-            loading={loading || searchLoading}
+            loading={loading}
             error={error}
           />
 
@@ -99,19 +72,6 @@ export default function Dictionary(): ReactElement {
           )}
         </>
       )}
-
-      {/* {after && (
-        <button
-          className="text-center bg-yellow rounded-full mx-auto flex my-4 py-2 px-4 text-sm"
-          onClick={() => {
-            return fetchMore({
-              variables: { cursor: after },
-            });
-          }}
-        >
-          load more
-        </button>
-      )} */}
     </>
   );
 }
